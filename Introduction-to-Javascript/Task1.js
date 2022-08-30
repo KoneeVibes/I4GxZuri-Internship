17fb771246b4702c3a8333f2154942adae56d3a3
Konee <104918313+KoneeVibes@users.noreply.github.com>
2022-08-30 13:49:11 -0500

    First JS task with Zuri

---
+++ Introduction-to-Javascript/Task1.js
@@ -0,0 +1,15 @@
+// Assignment
+
+// Write a javascript code that prints out your name, height, and country on the screen
+
+const myName = 'My name is Ofofonono,';
+var myHeight = 'Last I checked, I am 1.65m tall,';
+const myCountryOfOrigin = "I am from Nigeria, don't ask if I'm proud.";
+
+text1 = document.getElementById('text1');
+text2 = document.getElementById('text2');
+text3 = document.getElementById('text3');
+
+text1.innerHTML = myName;
+text2.innerHTML = myHeight;
+text3.innerHTML = myCountryOfOrigin;
